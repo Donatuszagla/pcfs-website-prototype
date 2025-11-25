4733ae8d9074564bdd73f2d31c5d7d7f9a1b1816
Donatus Zagla <beneesolomon765@gmail.com>
2025-11-25 16:12:24 +0000

    learning routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
+import { Media } from './media/media';
+import { Events } from './events/events';
 import { Branches } from './branches/branches';
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
@@ -5,5 +7,7 @@ import { Home } from './home/home';
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: Home },
-    { path: 'branches', component: Branches }
+    { path: 'branches', component: Branches },
+    { path: 'Events', component: Events },
+    { path: 'Media', component: Media },
 ];
